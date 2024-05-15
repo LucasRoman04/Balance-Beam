fc5c874d1f0e4124eeec8dc0585634073fcb9398
Veniot <w0404614@campus.nscc.ca>
2024-05-15 13:16:03 -0300

    Working, finer tuning next

--- Balance-Beam.ino
+++ Balance-Beam.ino
@@ -6,27 +6,21 @@
 #define MAX_DISTANCE 200
 #define SETPOINT 9
 
-//motor variables
-AF_Stepper motor(200, 2);
-int mode;
-int highSteps = 0;
-int steps = 0;
-int lowSteps = -50;
-const int STARTING_POINT = 48;
-
-//error variables
 float distance = 0;
 float error = 0;
 float prevError = 0;
-
-//sample time variables
+int highSteps = 100;  // Max amount of steps it should be able to take
+int steps = 0;        // Amount of steps it has taken
+int lowSteps = 0;     // Steps should not go below 0
 unsigned long previousMillis = 0;
+const int STARTING_POINT = 48;
 const long interval = 100;  // Interval in milliseconds
 
 //pid values
-float kp = 1.2;
+float kp = 1.4;  //1.7
 float ki = 0;
-float kd = 0;
+float kd = 300;  //300
+
 float pid_p;
 float pid_i;
 float pid_d;
@@ -35,55 +29,86 @@ float pid_total;
 // Create an Ultrasonic sensor object
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
-
+// Connect a stepper motor to motor port #2 (M3 and M4)
+AF_Stepper motor(200, 2);
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Stepper test!");
   motor.setSpeed(50);
-  motor.step(STARTING_POINT, BACKWARD);
+  // motor.step(STARTING_POINT, BACKWARD);  // Set bar to balance point off start
 }
 
 void loop() {
   unsigned long currentMillis = millis();
 
-  // Call the function to measure distance every 500 milliseconds
+  // Call every 100 milliseconds
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
+    measureDistance();
 
-    //measure distance from sensor, find how off it is from setpoint, apply PID
-    distance = sonar.ping_cm();
+    // PID calculations
     error = distance - SETPOINT;
     pid_p = error * kp;
     float distanceDifference = error - prevError;
     pid_d = kd * ((error - prevError) / interval);
+    pid_i = pid_i + (ki * error);
 
-    //if near the center add I, else dont
-    if (error < 3) {
-      pid_i += (ki * error);
-    } else {
-      pid_i = 0;
-    }
-    //sum pid values, save error
+    Serial.print("pid_i: ");
+    Serial.print(pid_i);
+
+    // // If centered don't add i or it will drift
+    // if (error > -3 && error < 3) {
+    //   pid_i = pid_i + (ki * error);
+    // } else {
+    //   pid_i = 0;
+    // }
+
+    // Calculate total PID output
     pid_total = pid_p + pid_i + pid_d;
-    prevError = error;
 
-    //set motor mode based on where the cart is on the rail
-    mode = distance < SETPOINT ? FORWARD : BACKWARD;
-    pid_total = abs(pid_total);
-
-    //apply pid output to motor
-    motor.step(pid_total, distance < SETPOINT ? FORWARD : BACKWARD);
-    Serial.print("distance: ");
-    Serial.print(distance);
-    Serial.print(" Speed: ");
+    Serial.print(" Pid total: ");
     Serial.println(pid_total);
 
+    // Save previous error
+    prevError = error;
 
-    //step code
-    // steps += mode == FORWARD ? pid_total : -pid_total;
-    // steps = constrain(steps, lowSteps, highSteps)
-    // Serial.print(" Steps: ");
-    // Serial.println(steps);
+    // Get motor direction
+    int mode = distance < SETPOINT ? FORWARD : BACKWARD;
+
+    // Dont let PID output go below 0
+    pid_total = pid_total < 0 ? -pid_total : pid_total;
+
+    // Motor reacting to cart distance
+    // Serial.println(mode);
+    // motor.step(1, mode);
+    if (distance < 10.5 && distance > 7.5) { pid_total = 0; }
+
+    motor.step(pid_total, distance < SETPOINT ? FORWARD : BACKWARD);
+
+
+
+    // Adjust steps and contrain them between min and max
+    steps += mode == FORWARD ? pid_total : -pid_total;
+    steps = constrain(steps, lowSteps, highSteps);
+
+    // Serial.print("    Direction: ");
+    // Serial.println(mode);
   }
+
+  // Data prints
+  // Serial.print("Steps: ");
+  // Serial.println(steps);
+  // Serial.print("PID: ");
+  // Serial.println(pid_total);
+  // Serial.print("Error: ");
+  // Serial.println(error);
+}
+
+void measureDistance() {
+  // Measure the distance using the ultrasonic sensor
+  distance = sonar.ping_cm();
+  // Serial.print("Distance: ");
+  // Serial.print(distance);
+  // Serial.print(" cm");
 }
